3d8c54d113811b62c948af3ca520ce916a76a8ee
Elliot Spargo <nspargo@andrew.cmu.edu>
2021-03-17 15:06:43 -0400

    theta!

--- trans.v
+++ trans.v
@@ -1,5 +1,10 @@
-From Karl Require Sigma. Tactics Sequence
+From istari Require Import Sigma Tactics
      Syntax Subst SimpSub Promote Hygiene
      ContextHygiene Equivalence Rules.
 
-Definition theta A B :
+Definition theta : (term False) :=
+  lam(
+      lam (
+ app (var 1) (app (app (var 2) (var 2)) (var 1))
+        )
+    ).
