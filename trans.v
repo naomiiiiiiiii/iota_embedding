b5503dced0908c6fab0eca80dc2cd164710912aa
Elliot Spargo <nspargo@andrew.cmu.edu>
2021-03-18 16:15:33 -0400

    bind

--- trans.v
+++ trans.v
@@ -2,29 +2,56 @@ From istari Require Import Sigma Tactics
      Syntax Subst SimpSub Promote Hygiene
      ContextHygiene Equivalence Rules.
 
+Definition plus L R : (term False) := sigma booltp (bite (var 0) (subst (sh 1) L)
+                                           (subst (sh 1) R) ).
+Definition inl L: term False := ppair btrue L.
+Definition inr R: term False := ppair bfalse R.
+
 Definition theta : (term False) :=
   lam(
       lam (
- app (var 1) (app (app (var 2) (var 2)) (var 1))
+ app (var 0) (app (app (var 1) (var 1)) (var 0))
         )
     ).
 
 Definition combt A: (term False) := rec (
-                                    pi (fut (var 1)) (subst (sh 1) A)).
+                                    pi (fut (var 0)) (subst (sh 1) A)).
 (*probably have to shift A cuz its going under a binder*)
 
-Definition Yc A: (term False) = lam ((*f := 1*)
+Definition Yc: (term False) := lam ((*f := 0*)
                    app
-               (lam (fut (comb A)) (
-                                    next ( (comb A)) x
-                ([x']
-                app f (next (app x' x)))
-                ))
+                     (lam ( (*f := 1, x := 0*)
+                          let f := (var 1) in
+                          let x := (var 0) in
+                app f (next (app (prev x) x))
+                        )
+                     )
                 (next
-               (lam (later (comb A)) ([x]
-                                    letnext (later (comb A)) x
-                ([x']
-                   app f (next (app x' x)))
-                 )))
+                   (lam (*f := 1, x := 0*)
+                      (
+                          let f := (var 1) in
+                          let x := (var 0) in
+                   app f (next (app (prev x) x))
+                      )
+                ))
 ).
+
+Definition laters A : term False := rec (plus A (fut (var 0))).
+  (*notice similarity w type of nat*)
+
+Definition ret: term False := lam (inl (var 0)).
+
+Definition bind : term False := app Yc
+   (lam   ( (*f := 0*)
+      lam ( (*f:= 1, x := 0*)
+          lam ( (*f:= 2, x := 1, g := 0*)
+              let f := (var 2) in
+              let x := (var 1) in
+              let g := (var 0) in
+              let y := ppi2 x in
+         bite (ppi1 x) (app g y)
+              (let y' := prev y in
+               let f' := prev f in
+               inr (next (app (app f' y') g))) )
+        ))).
 
