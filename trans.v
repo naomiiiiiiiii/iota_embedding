5334635c1d9cd48c4c87f98fc0992a6f43221de9
Elliot Spargo <nspargo@andrew.cmu.edu>
2021-03-18 09:51:14 -0400

    variables

--- trans.v
+++ trans.v
@@ -8,3 +8,23 @@ Definition theta : (term False) :=
  app (var 1) (app (app (var 2) (var 2)) (var 1))
         )
     ).
+
+Definition combt A: (term False) := rec (
+                                    pi (fut (var 1)) (subst (sh 1) A)).
+(*probably have to shift A cuz its going under a binder*)
+
+Definition Yc A: (term False) = lam ((*f := 1*)
+                   app
+               (lam (fut (comb A)) (
+                                    next ( (comb A)) x
+                ([x']
+                app f (next (app x' x)))
+                ))
+                (next
+               (lam (later (comb A)) ([x]
+                                    letnext (later (comb A)) x
+                ([x']
+                   app f (next (app x' x)))
+                 )))
+).
+
