ca3f5b5ad36fb31bf651d9aad9c96f3cf323284f
Elliot Spargo <nspargo@andrew.cmu.edu>
2021-04-01 19:11:14 -0400

    trans?

--- trans.v
+++ trans.v
@@ -1,9 +1,60 @@
+From istari Require Import source subst_src rules_src help.
 From istari Require Import Sigma Tactics
      Syntax Subst SimpSub Promote Hygiene
      ContextHygiene Equivalence Rules Defined.
-From istari Require Import source subst_src rules_src help.
 
 Inductive trans_type: (source.term False) -> (Syntax.term False) -> Type :=
-  tt_ref: forall A As, trans_type A As -> trans_type (reftp_m A) (make_ref As).
+  tt_ref: forall A As, trans_type A As -> trans_type (reftp_m A) (make_ref As)
+| tt_comp: forall T Ts, trans_type T Ts -> trans_type (comp_m T)
+                                                (lam (*w:= var 0*)
+                                                (all world one ((*w = 1, u := var 0*)
+                                                       let w := Syntax.var 1 in
+                                                       let u := Syntax.var 0 in
+    arrow (subseq w u) (arrow (store u)
+                         (laters (exist world one ((*w:= 2, u:= 1, u':= 0*)
+                                              let u := Syntax.var 1 in
+                                              let u' := Syntax.var 0 in
+                                                    prod (prod (subseq u u') (store u'))
+                                                    (app Ts u'))
+                                    )
+                       ))
+                                                ))
+                                                ).
 
-Inductive trans
+Inductive trans source.context -> (source.term False) -> (Syntax.term False) -> Type :=
+  t_ref: trans E Es ->
+         trans_type T Ts ->
+         tr G (oof_m E T) ->
+         trans (ref E)
+               (*unresolved question: need i ever use x?
+                yes but move Es straight to u4 m ' o m o m0*)
+lam ( (*u1  = 0*)
+               ( lam (*u1 = 1 m = 0*)
+                       (  lam (*u1 = 2m = 1, s= 0*)
+                            (let m := var 1 in
+                             let s := var 0 in
+                             let x := app (app (move_context G) triv) Es in
+                             ret (ppair (ppair triv (lam
+                                                       (*u1 = 3 m = 2 s  = 1 m' = 0*)
+(ppair (app s triv) (next (app (move_app T) (app Es u1))))
+                                                    )
+                                 ) )
+                             (exist world
+        ([u']
+         prod (prod (subseq u2 u')
+         (store u')) ((make_ref Ts) u'))))   type
+(ppair (ppair
+       triv   u2 <:= x::u2
+        (all world ([uf] lam (subseq (cons (lam (later world)
+                                                 ([u] letnext world u ([u'] later(Ts u')))) u2)
+                                  uf)
+([m'] (ppair (app s triv))
+         (ES uf)))
+)
+                      )   store
+(ppair (app length u2) (all world ([_] triv)))
+)
+)))
+                        <- of E T
+                        <- trans_type T Ts
+                        <- trans E ES.
